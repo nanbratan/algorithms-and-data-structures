62f588718d5a8ecd982f4e9e63c159b1a66c0003
nanbratan <4fc1e124-2aa9-413d-b5d6-5cdc468ad1a1@nanbratan.com>
2024-08-09 16:50:25 +1000

    Moved common Graph behaviour to traits(Graph and GraphNode traits). Now it's possible to make more different graphs(even trees) with these traits and use existing breadth_first_search for new graphs/trees without any adjustments.

--- src/algorithms/breadth_first_search.rs
+++ src/algorithms/breadth_first_search.rs
@@ -1,4 +1,4 @@
-use crate::graph::{Graph, Node};
+use crate::graph::{Graph, GraphNode};
 use crate::Queue;
 use std::collections::HashMap;
 use std::fmt::Debug;
@@ -18,34 +18,37 @@ use std::rc::Rc;
 ///
 /// # Complexity
 /// This algorithm has `O(n * e)` complexity, where `n` is a number of nodes and `e` is a number of edges(connections between nodes).
-pub fn breadth_first_search<K, T, P>(
+pub fn breadth_first_search<K, G, N, T, P>(
     start_node_id: K,
-    graph: &Graph<T, K>,
+    graph: &G,
     predicate: P,
-) -> Option<&Rc<Node<T, K>>>
+) -> Option<&Rc<N>>
 where
     T: Debug,
+    G: Graph<N, K>,
+// Debug trait only for Drop trait visualisation, it should be removed if visualisation is not needed
+    N: GraphNode<Value=T> + Debug,
     K: Eq + Hash + Copy + Debug,
     P: Fn(&T) -> bool,
 {
     let mut checked_nodes = HashMap::with_capacity(graph.len());
     let head_node = graph.get(&start_node_id)?;
-    let mut queue = Queue::from(head_node.nodes.as_ref()?);
+    let mut queue = Queue::from(head_node.nodes().as_ref()?);
 
     while let Some(queue_item) = queue.take() {
         // Different nodes may point to a same node, so to avoid extra check of already checked nodes - we log them and skip them
         // It also prevents infinity loop in case if we have 2 nodes which points to each other
-        if checked_nodes.contains_key(&queue_item.id) {
+        if checked_nodes.contains_key(queue_item.id()) {
             continue;
         }
 
-        if predicate(&queue_item.item) {
+        if predicate(queue_item.value()) {
             return Some(queue_item);
         }
 
-        checked_nodes.insert(&queue_item.id, true);
+        checked_nodes.insert(queue_item.id(), true);
 
-        if let Some(nodes) = &queue_item.nodes {
+        if let Some(nodes) = &queue_item.nodes() {
             queue.append(nodes);
         }
     }
@@ -57,58 +60,40 @@ where
 mod tests {
     use std::rc::Rc;
 
-    use crate::algorithms::breadth_first_search::{breadth_first_search, Node};
-    use crate::graph::Graph;
+    use crate::algorithms::breadth_first_search::breadth_first_search;
+    use crate::graph::{BasicGraph, BasicGraphNode, Graph, GraphNode};
 
     #[derive(Debug)]
     struct Item(bool);
 
     #[test]
     fn should_find_shortest() {
-        let mut graph = Graph::new();
+        let mut graph = BasicGraph::new();
 
-        let head_id = 1;
-
-        let eight = Rc::new(Node {
-            id: 8,
-            item: Item(false),
-            nodes: None,
-        });
-        let seven = Rc::new(Node {
-            id: 7,
-            item: Item(true),
-            nodes: None,
-        });
-        let six = Rc::new(Node {
-            id: 6,
-            item: Item(false),
-            nodes: Some(vec![Rc::clone(&eight)]),
-        });
-        let five = Rc::new(Node {
-            id: 5,
-            item: Item(false),
-            nodes: None,
-        });
-        let four = Rc::new(Node {
-            id: 4,
-            item: Item(false),
-            nodes: None,
-        });
-        let three = Rc::new(Node {
-            id: 3,
-            item: Item(false),
-            nodes: Some(vec![Rc::clone(&six), Rc::clone(&seven), Rc::clone(&five)]),
-        });
-        let two = Rc::new(Node {
-            id: 2,
-            item: Item(false),
-            nodes: Some(vec![Rc::clone(&four), Rc::clone(&five)]),
-        });
-        let one = Rc::new(Node {
-            id: head_id,
-            item: Item(false),
-            nodes: Some(vec![Rc::clone(&two), Rc::clone(&three)]),
-        });
+        let eight = Rc::new(BasicGraphNode::new(8, Item(false), None));
+        let seven = Rc::new(BasicGraphNode::new(7, Item(true), None));
+        let six = Rc::new(BasicGraphNode::new(
+            6,
+            Item(false),
+            Some(vec![Rc::clone(&eight)]),
+        ));
+        let five = Rc::new(BasicGraphNode::new(5, Item(false), None));
+        let four = Rc::new(BasicGraphNode::new(4, Item(false), None));
+        let three = Rc::new(BasicGraphNode::new(
+            3,
+            Item(false),
+            Some(vec![Rc::clone(&six), Rc::clone(&seven), Rc::clone(&five)]),
+        ));
+        let two = Rc::new(BasicGraphNode::new(
+            2,
+            Item(false),
+            Some(vec![Rc::clone(&four), Rc::clone(&five)]),
+        ));
+        let one = Rc::new(BasicGraphNode::new(
+            1,
+            Item(false),
+            Some(vec![Rc::clone(&two), Rc::clone(&three)]),
+        ));
 
         graph.insert(eight);
         graph.insert(seven);
@@ -119,33 +104,20 @@ mod tests {
         graph.insert(two);
         graph.insert(one);
 
-        assert_eq!(
-            7,
-            breadth_first_search(head_id, &graph, |x| x.0).unwrap().id
-        )
+        assert_eq!(&7, breadth_first_search(1, &graph, |x| x.0).unwrap().id())
     }
 
     #[test]
     fn should_not_find_anything() {
-        let mut graph = Graph::new();
+        let mut graph = BasicGraph::new();
 
-        let head_id = 1;
-
-        let three = Rc::new(Node {
-            id: 3,
-            item: Item(false),
-            nodes: None,
-        });
-        let two = Rc::new(Node {
-            id: 2,
-            item: Item(false),
-            nodes: None,
-        });
-        let one = Rc::new(Node {
-            id: head_id,
-            item: Item(false),
-            nodes: Some(vec![Rc::clone(&two), Rc::clone(&three)]),
-        });
+        let three = Rc::new(BasicGraphNode::new(3, Item(false), None));
+        let two = Rc::new(BasicGraphNode::new(2, Item(false), None));
+        let one = Rc::new(BasicGraphNode::new(
+            1,
+            Item(false),
+            Some(vec![Rc::clone(&two), Rc::clone(&three)]),
+        ));
 
         graph.insert(one);
         graph.insert(three);
--- src/data_structures/graph.rs
+++ src/data_structures/graph.rs
@@ -1,46 +1,107 @@
+// TODO: Remove line below when figure out better files structure to avoid functions with the same name as modules
+#![allow(clippy::module_name_repetitions)]
+
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::rc::Rc;
-#[derive(Debug)]
-pub struct Node<T, K> {
-    pub id: K,
-    pub item: T,
-    pub nodes: Option<Vec<Rc<Node<T, K>>>>,
+
+///
+/// Graph traits
+///
+
+pub trait GraphNode
+where
+    Self::Id: Hash + Eq,
+{
+    type Id;
+    type Value;
+
+    fn id(&self) -> &Self::Id;
+    fn value(&self) -> &Self::Value;
+    fn nodes(&self) -> &Option<Vec<Rc<Self>>>;
 }
 
-pub struct Graph<T, K = i32>(HashMap<K, Rc<Node<T, K>>>);
 
-impl<T, K> Graph<T, K>
+pub trait Graph<Node, Key>
+where
+    Node: GraphNode,
+{
+    fn insert(&mut self, node: Rc<Node>);
+    fn get(&self, node_id: &Key) -> Option<&Rc<Node>>;
+    fn len(&self) -> usize;
+    fn is_empty(&self) -> bool;
+}
+
+///
+/// Basic graph implementation
+///
+
+#[derive(Debug)]
+pub struct BasicGraphNode<T, K> {
+    id: K,
+    value: T,
+    nodes: Option<Vec<Rc<BasicGraphNode<T, K>>>>,
+}
+
+impl<T, K> BasicGraphNode<T, K> {
+    #[must_use]
+    pub fn new(id: K, value: T, nodes: Option<Vec<Rc<BasicGraphNode<T, K>>>>) -> Self {
+        Self { id, value, nodes }
+    }
+}
+
+impl<T, K> GraphNode for BasicGraphNode<T, K>
+where
+    K: Hash + Eq,
+{
+    type Id = K;
+    type Value = T;
+
+    #[must_use]
+    fn id(&self) -> &Self::Id {
+        &self.id
+    }
+    #[must_use]
+    fn value(&self) -> &Self::Value {
+        &self.value
+    }
+    #[must_use]
+    fn nodes(&self) -> &Option<Vec<Rc<Self>>> {
+        &self.nodes
+    }
+}
+
+pub struct BasicGraph<T, K = i32>(HashMap<K, Rc<BasicGraphNode<T, K>>>);
+
+impl<T, K> BasicGraph<T, K> {
+    #[must_use]
+    pub fn new() -> Self {
+        BasicGraph(HashMap::new())
+    }
+}
+
+impl<T, K> Graph<BasicGraphNode<T, K>, K> for BasicGraph<T, K>
 where
     K: Eq + Hash + Copy,
 {
-    #[must_use]
-
-    pub fn new() -> Self {
-        Graph(HashMap::new())
-    }
-
-    pub fn insert(&mut self, node: Rc<Node<T, K>>) {
+    fn insert(&mut self, node: Rc<BasicGraphNode<T, K>>) {
         self.0.insert(node.id, node);
     }
-
     #[must_use]
-    pub fn get(&self, node_id: &K) -> Option<&Rc<Node<T, K>>> {
+    fn get(&self, node_id: &K) -> Option<&Rc<BasicGraphNode<T, K>>> {
         self.0.get(node_id)
     }
-
     #[must_use]
-    pub fn len(&self) -> usize {
+    fn len(&self) -> usize {
         self.0.len()
     }
-
     #[must_use]
-    pub fn is_empty(&self) -> bool {
+    fn is_empty(&self) -> bool {
         self.0.is_empty()
     }
 }
 
-impl<T, K> Default for Graph<T, K>
+impl<T, K> Default for BasicGraph<T, K>
 where
     K: Eq + Hash + Copy,
 {
