da7bd510e3211bd47ecd930470972a8d9edc75a0
nanbratan <4fc1e124-2aa9-413d-b5d6-5cdc468ad1a1@nanbratan.com>
2024-08-25 09:19:05 +1000

    Finished k nearest neighbor(KNN) algorithm

--- src/algorithms/k_nearest_neighbor.rs
+++ src/algorithms/k_nearest_neighbor.rs
@@ -1,27 +1,74 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
 
-trait Neighbor {
-    fn calculate_neighbor_distance(&self, another_neighbor: &Self) -> i32;
+pub trait Neighbor {
+    fn calculate_neighbor_distance(&self, another_neighbor: &Self) -> f64;
 }
 
+struct NeighborWithDistance<'a> {
+    pub neighbor_name: &'a str,
+    pub distance: f64,
+}
+
+impl<'a> Eq for NeighborWithDistance<'a> {}
+
+impl<'a> PartialEq<Self> for NeighborWithDistance<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.distance == other.distance
+    }
+}
+
+impl<'a> PartialOrd<Self> for NeighborWithDistance<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> Ord for NeighborWithDistance<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.distance > other.distance {
+            Ordering::Greater
+        } else if self.distance < other.distance {
+            Ordering::Less
+        } else {
+            Ordering::Equal
+        }
+    }
+}
+
+// TODO: Docs
 pub fn k_nearest_neighbor<'a, T: Neighbor>(
     neighbors: &'a HashMap<&str, T>,
-    item_name: &str,
+    item_name: &'static str,
     neighbors_count: usize,
 ) -> Vec<&'a str> {
-    let mut neighbors_with_distance: Vec<(&str, i32)> = Vec::with_capacity(neighbors.len());
+    let mut priority_queue: BinaryHeap<NeighborWithDistance> =
+        BinaryHeap::with_capacity(neighbors_count);
     let item = neighbors.get(item_name).unwrap();
 
-    for (&name, neighbor) in neighbors.iter() {
-        let distance = neighbor.calculate_neighbor_distance(item);
-        neighbors_with_distance.push((name, distance));
+    for (&name, neighbor) in neighbors {
+        if name == item_name {
+            continue;
+        }
+
+        let next_neighbor = NeighborWithDistance {
+            neighbor_name: name,
+            distance: neighbor.calculate_neighbor_distance(item),
+        };
+
+        if priority_queue.len() == neighbors_count {
+            let mut biggest_item = priority_queue.peek_mut().unwrap();
+            if biggest_item.distance > next_neighbor.distance {
+                *biggest_item = next_neighbor;
+            }
+        } else {
+            priority_queue.push(next_neighbor);
+        }
     }
 
-    neighbors_with_distance.sort_by(|a, b| a.1.cmp(&b.1));
-    neighbors_with_distance.shrink_to(neighbors_count);
-    neighbors_with_distance
-        .iter()
-        .map(|neighbor| neighbor.0)
+    priority_queue
+        .into_iter()
+        .map(|neighbor| neighbor.neighbor_name)
         .collect()
 }
 
@@ -38,14 +85,14 @@ mod tests {
         romance: i32,
     }
 
-    struct Viewer {
-        name: &'static str,
+    struct Viewer<'a> {
+        name: &'a str,
         preferences: Preferences,
     }
 
-    impl Viewer {
+    impl<'a> Viewer<'a> {
         fn new(
-            name: &'static str,
+            name: &'a str,
             comedy: i32,
             action: i32,
             drama: i32,
@@ -65,8 +112,8 @@ mod tests {
         }
     }
 
-    impl Neighbor for Viewer {
-        fn calculate_neighbor_distance(&self, another_neighbor: &Self) -> i32 {
+    impl<'a> Neighbor for Viewer<'a> {
+        fn calculate_neighbor_distance(&self, another_neighbor: &Self) -> f64 {
             let comedy_difference =
                 (&self.preferences.comedy - another_neighbor.preferences.comedy).pow(2);
             let action_difference =
@@ -83,12 +130,13 @@ mod tests {
                 + drama_difference
                 + horror_difference
                 + romance_difference) as f64)
-                .sqrt() as i32
+                .sqrt()
         }
     }
 
     #[test]
     fn should_find_three_nearest() {
+        // given
         let bob = Viewer::new("bob", 3, 4, 4, 1, 4);
         let margie = Viewer::new("margie", 4, 3, 5, 1, 5);
         let john = Viewer::new("john", 2, 5, 1, 3, 1);
@@ -104,9 +152,11 @@ mod tests {
         neighbors.insert("tom", tom);
         neighbors.insert("jared", jared);
 
+        // when
         let three_nearest_neighbors = k_nearest_neighbor(&neighbors, "margie", 3);
 
-        assert_eq!(vec!["jared"], three_nearest_neighbors);
+        // then
+        assert_eq!(3, three_nearest_neighbors.len());
         assert!(three_nearest_neighbors.contains(&"bob"));
         assert!(three_nearest_neighbors.contains(&"jared"));
         assert!(three_nearest_neighbors.contains(&"cristy"));
